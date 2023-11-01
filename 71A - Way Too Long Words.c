220f9062a77d9a344935bb49c619ac8b76866355
Tasnimul Hasan <hawkeye00747007@gmail.com>
2023-11-01 13:34:03 +0600

    not long enough

---
+++ 71A - Way Too Long Words.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    char word[100];
+    scanf("%i", &n);
+    for(int i = 0; i < n; i++)
+    {
+        int count = 0;
+        scanf("%s", word);
+        for(int i = 0; i < 100; i++)
+            if(word[i] != '\0')
+                count++;
+            else
+                break;
+        if(count <= 10)
+            printf("%s\n", word);
+        else
+            printf("%c%i%c\n", word[0], count - 2, word[count - 1]);
+    }
+}
